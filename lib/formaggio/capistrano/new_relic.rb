b71211ab51327eeea6e85841b919398046be2c81
Barnaby <barnaby.alter@nyu.edu>
2014-10-13 14:07:28 -0400

    Changed nyulibraries:deploy namespace to match the new formaggio one

--- lib/formaggio/capistrano/new_relic.rb
+++ lib/formaggio/capistrano/new_relic.rb
@@ -6,25 +6,25 @@ Capistrano::Configuration.instance(:must_exist).load do
   # After newrelic runs, reset up its yaml file
   after   "newrelic:notice_deployment", "newrelic:reset"
   after   "deploy:update",              "newrelic:notice_deployment"
-  
+
 
   namespace :newrelic do
     def new_relic_environment?
       return false if fetch(:new_relic_environments).nil?
       fetch(:new_relic_environments).collect {|environment| environment.to_sym}.include?(fetch(:stage, fetch(:rails_env, :staging)).to_sym)
     end
-    
+
     desc "Write New Relic file without ERB for processing by New Relic rpm recipe"
     task :set do
       if new_relic_environment?
-        run_locally "bundle exec rake nyulibraries:deploy:newrelic:set RAILS_ENV=#{fetch(:rails_env, 'staging')}"
+        run_locally "bundle exec rake formaggio:newrelic:set RAILS_ENV=#{fetch(:rails_env, 'staging')}"
       end
     end
 
     desc "Reset the New Relic file"
     task :reset do
       if new_relic_environment?
-        run_locally "bundle exec rake nyulibraries:deploy:newrelic:reset RAILS_ENV=#{fetch(:rails_env, 'staging')} "
+        run_locally "bundle exec rake formaggio:newrelic:reset RAILS_ENV=#{fetch(:rails_env, 'staging')} "
       end
     end
   end
--- lib/formaggio/capistrano/server/puma.rb
+++ lib/formaggio/capistrano/server/puma.rb
@@ -3,21 +3,21 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Start the application"
     task :start do
       fetch(:puma_ports, [9000]).each do |port|
-        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake nyulibraries:deploy:puma:start[#{port}]"
+        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake formaggio:puma:start[#{port}]"
       end
     end
-  
+
     desc "Stop the application"
     task :stop do
       fetch(:puma_ports, [9000]).each do |port|
-        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake nyulibraries:deploy:puma:stop[#{port}]"
+        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake formaggio:puma:stop[#{port}]"
       end
     end
-  
+
     desc "Restart the application"
     task :restart, :roles => :app, :except => { :no_release => true } do
       fetch(:puma_ports, [9000]).each do |port|
-        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake nyulibraries:deploy:puma:restart[#{port}]"
+        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake formaggio:puma:restart[#{port}]"
       end
     end
   end
