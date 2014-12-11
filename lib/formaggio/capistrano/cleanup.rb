2241f9570f0dee1f18142a3dcc9a05410ebe5ce6
hab278 <hab278@nyu.edu>
2014-12-11 13:48:03 -0500

    Fix wrong name for task

--- lib/formaggio/capistrano/cleanup.rb
+++ lib/formaggio/capistrano/cleanup.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   after "deploy:restart", "deploy:cleanup"
   before "deploy:cleanup", "touch:latest_release"
   namespace :touch do
-    task :set_variables do
+    task :latest_release do
       run "touch #{current_release}"
     end
   end
