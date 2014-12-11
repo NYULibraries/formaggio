e8a7e07893db4695e8de14dbfb9b96cf19bc6e4c
hab278 <hab278@nyu.edu>
2014-12-11 13:09:15 -0500

    Make sure latest release is the most recently modified release

--- lib/formaggio/capistrano/cleanup.rb
+++ lib/formaggio/capistrano/cleanup.rb
@@ -1,3 +1,9 @@
 Capistrano::Configuration.instance(:must_exist).load do
   after "deploy:restart", "deploy:cleanup"
+  before "deploy:cleanup", "touch:latest_release"
+  namespace :touch do
+    task :set_variables do
+      run "touch #{current_release}"
+    end
+  end
 end
