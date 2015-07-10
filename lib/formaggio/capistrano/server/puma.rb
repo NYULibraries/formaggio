d089f84065352ad7e41d7ca68a2a2eff25277e1d
hab278 <hab278@nyu.edu>
2015-07-10 15:47:53 -0400

    Now we can use puma ssl with cap tasks too

--- lib/formaggio/capistrano/server/puma.rb
+++ lib/formaggio/capistrano/server/puma.rb
@@ -3,22 +3,26 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Start the application"
     task :start do
       fetch(:puma_ports, [9000]).each do |port|
-        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake formaggio:puma:start[#{port}]"
+        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake formaggio:puma:start[#{puma_args(port)}]"
       end
     end
 
     desc "Stop the application"
     task :stop do
       fetch(:puma_ports, [9000]).each do |port|
-        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake formaggio:puma:stop[#{port}]"
+        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake formaggio:puma:stop[#{puma_args(port)}]"
       end
     end
 
     desc "Restart the application"
     task :restart, :roles => :app, :except => { :no_release => true } do
       fetch(:puma_ports, [9000]).each do |port|
-        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake formaggio:puma:restart[#{port}]"
+        run "cd #{current_release} && RAILS_ENV=#{fetch(:rails_env, 'staging')} bundle exec rake formaggio:puma:restart[#{puma_args(port)}]"
       end
+    end
+
+    def puma_args(port)
+      "#{port}#{fetch(:puma_ssl_enabled, false) ? ",'ssl'" : ""}"
     end
   end
 end
