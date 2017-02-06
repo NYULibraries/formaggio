107304838b2bb6a5f3135ed0bd2e97fdb9c1dba5
hab278 <hab278@nyu.edu>
2017-02-06 14:44:53 -0500

    Testing for using puma threads

--- spec/formaggio/puma_config_spec.rb
+++ spec/formaggio/puma_config_spec.rb
@@ -24,6 +24,7 @@ module Formaggio
           puma_config.host_ip.should eq("127.0.0.1")
           puma_config.keystore.should eq(ENV["KEYSTORE"])
           puma_config.keystore_pass.should eq(ENV["KEYSTORE_PASS"])
+          puma_config.threads.should eq('2:4')
           puma_config.bind.should eq("'ssl://127.0.0.1:#{port}?keystore=&keystore-pass=&verify_mode=none'")
           puma_config.pid.should eq("#{dummy_path}/tmp/pids/puma-#{port}.pid")
           puma_config.log.should eq("#{dummy_path}/log/puma-test-#{port}.log")
@@ -34,16 +35,23 @@ module Formaggio
         let(:host_ip)       { "127.0.0.10" }
         let(:keystore)      { "keystore" }
         let(:keystore_pass) { "pass" }
-        let(:puma_options)  { { ssl_enabled: true,
+        let(:threads)       { '2:12' }
+        let(:puma_options)  {
+                              {
+                                ssl_enabled: true,
                                 host_ip: host_ip,
                                 keystore: keystore,
-                                keystore_pass: keystore_pass } }
+                                keystore_pass: keystore_pass,
+                                threads: threads
+                              }
+                            }
 
         it "returns correct settings based on config when ssl" do
           puma_config.port.should eq(port)
           puma_config.host_ip.should eq(host_ip)
           puma_config.keystore.should eq(keystore)
           puma_config.keystore_pass.should eq(keystore_pass)
+          puma_config.threads.should eq('2:12')
           puma_config.bind.should eq("'ssl://#{host_ip}:#{port}?keystore=#{keystore}&keystore-pass=#{keystore_pass}&verify_mode=none'")
           puma_config.pid.should eq("#{dummy_path}/tmp/pids/puma-#{port}.pid")
           puma_config.log.should eq("#{dummy_path}/log/puma-test-#{port}.log")
